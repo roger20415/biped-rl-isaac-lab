374f06a81b4205e1c00635989c5fa575c17493e1
roger20415 <roger20415@gmail.com>
2025-10-20 03:52:32 +0000

    [FEAT] add run_biped_rl_env.py

---
+++ run_biped_rl_env.py
@@ -0,0 +1,61 @@
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on running the bipedal walking RL environment.")
+parser.add_argument("--num_envs", type=int, default=16, help="Number of environments to spawn.")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+
+from isaaclab.envs import ManagerBasedRLEnv
+
+from isaaclab_tasks.manager_based.biped_rl.biped_rl_env_cfg import BipedRlEnvCfg
+
+
+def main():
+    """Main function."""
+    # create environment configuration
+    env_cfg = BipedRlEnvCfg()
+    env_cfg.scene.num_envs = args_cli.num_envs
+    env_cfg.sim.device = args_cli.device
+    # setup RL environment
+    env = ManagerBasedRLEnv(cfg=env_cfg)
+
+    # simulate physics
+    count = 0
+    while simulation_app.is_running():
+        with torch.inference_mode():
+            # reset
+            if count % 300 == 0:
+                count = 0
+                env.reset()
+                print("-" * 80)
+                print("[INFO]: Resetting environment...")
+            # sample random actions
+            joint_efforts = torch.randn_like(env.action_manager.action)
+            # step the environment
+            obs, rew, terminated, truncated, info = env.step(joint_efforts)
+            # update counter
+            count += 1
+
+    # close the environment
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
