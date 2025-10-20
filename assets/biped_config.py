faaee04416bf729563d796f1d7842c666ba54c4b
roger20415 <roger20415@gmail.com>
2025-10-20 13:59:01 +0800

    [FEAT] add biped assets
    - biped.usd
    - biped_config.py

---
+++ assets/biped_config.py
@@ -0,0 +1,29 @@
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.sim import UsdFileCfg
+
+USD_PATH = "/workspace/isaaclab/scripts/demos/biped.usd"
+
+BIPED_CFG = ArticulationCfg(
+    spawn=UsdFileCfg(
+        usd_path=USD_PATH,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=None,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=None,
+            solver_velocity_iteration_count=None,
+        ),
+    ),
+
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.002),
+    ),
+
+    actuators={
+        "joints_actuators": ImplicitActuatorCfg(joint_names_expr=[".*"], damping=None, stiffness=None)
+    },
+)
